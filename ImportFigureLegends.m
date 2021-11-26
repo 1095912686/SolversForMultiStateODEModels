4c133bee84e8283c2c8e8a2a253bf351f0bfa19f
Guo Xiaohao <30867091+1095912686@users.noreply.github.com>
2021-11-26 11:02:51 +0800

    Add files via upload

---
+++ ImportFigureLegends.m
@@ -0,0 +1,14 @@
+%%% groupLegend
+groupLegend = cell(n,1);
+for i = 1:n
+    groupLegend{i,1} = ['group ',num2str(i)];
+end
+
+%%% ageLegends
+ageLegend = {'0 to 9','10 to 19','20 to 29', '30 to 39',...
+             '40 to 49', '50 to 59', '60 to 69', '\geq 70' };
+
+
+
+%%% doseLegends
+doseLegend = {'none', '1 dose', '2 doses', '3 doses'};
